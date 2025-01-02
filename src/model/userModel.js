4636b64836fca12c021f9efa957b2543f48c4a7d
Nikhiil-esp <rnikhil@espsofttech.org>
2025-01-02 10:57:43 +0530

    seconde commit

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -61,5 +61,6 @@ userSchema.methods.GenerateRefreshToke = async function() {
     },process.env.REFRESH_TOKEN_KEY,{expiresIn:"10d"})
 }
 console.log("nikhil")
+console.log("arajput")
 const userModel = mongoose.model("user",userSchema)
 export default userModel
